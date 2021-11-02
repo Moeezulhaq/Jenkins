418c532e4394bb1742f7089e6caaf128e059116c
Moeezulhaq <moeez.ulhaq@eurustechnologies.com>
2021-11-02 13:10:23 -0700

    changes

--- scripts/deploy-stack.sh
+++
@@ -1,6 +0,0 @@
-    aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file $TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION
